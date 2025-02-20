218219a5c68e53ecd96781a82e4ba55fc019f185
NagaJahnaviD <nagajahnavidannayak@gmail.com>
2025-02-20 08:53:39 +0530

    added caht

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -4,18 +4,23 @@ export default function Chat()
     return (
         <div className='flex h-screen'>
             <div className="bg-blue-100 w-1/3">
-
+                contacts
             </div>
-            <div className="bg-blue-300 w-2/3">
+            <div className="bg-blue-300 w-2/3 p-2">
             <div>msg with selected person</div>
-                <div className='flex'>
-                    <input type="text" 
-                           placeholder='Your message' 
-                           className='bg-white border p-2' />
-                    <button className='bg-blue-600'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
-                    </svg>........
+                <div className='flex gap-2 w-full'>
+                    <input type="text" placeholder='Type your message here' 
+                    className="bg-white  flex-grow border p-2" />
+                    <button type="submit" className="bg-black p-2 text-white rounded-sm w-10 h-10 flex items-center justify-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" 
+                                    fill="none" 
+                                    viewBox="0 0 24 24" 
+                                    strokeWidth={1.5} 
+                                    stroke="currentColor" 
+                                    className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" 
+                                        d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                                </svg>
                     </button>
                 </div>
             </div>
